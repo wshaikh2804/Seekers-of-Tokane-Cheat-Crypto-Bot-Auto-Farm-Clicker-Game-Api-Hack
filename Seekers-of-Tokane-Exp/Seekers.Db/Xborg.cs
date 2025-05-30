35efce68d853b39e2e0c4c066d3979e5e56e9d99
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:02:15 +0300

    Xborg.cs

---
+++ Seekers-of-Tokane-Exp/Seekers.Db/Xborg.cs
@@ -0,0 +1,36 @@
+﻿using DogsHouse.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogsHouse.Db
+{
+    public class DogsHouseDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DogsHouseDbContext() { }
+
+        public DogsHouseDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        #region DbSets
+        public DbSet<Dog> Dogs { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+                optionsBuilder.UseLazyLoadingProxies();
+
+            }
+        }
+    }
+}
