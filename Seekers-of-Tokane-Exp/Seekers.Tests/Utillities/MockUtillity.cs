3bd8ab8bc2e690b2af26d9038dda2174d64dba22
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:03:52 +0300

    MockUtillity.cs

---
+++ Seekers-of-Tokane-Exp/Seekers.Tests/Utillities/MockUtillity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace DogHouse.Tests.Utillities
+{
+    public class MockUtillity
+    {
+        public static Mock<DbSet<T>> GetMockedDbSet<T>(List<T> list) where T : class
+        {
+            var queryable = list.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+
+            return dbSet;
+        }
+    }
+}
