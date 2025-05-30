7ed3928c6f57efb00f10b408f9c906f7c231db63
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:02:12 +0300

    IDogService.cs

---
+++ Seekers-of-Tokane-Exp/Seekers-App/Services/IDogService.cs
@@ -0,0 +1,14 @@
+﻿using DogsHouse.Db.Entities;
+using DogsHouseApp.Models;
+
+namespace DogsHouseApp.Services
+{
+    public interface IDogService
+    {
+        Task AddDogAsync(DogModel dogModel);
+
+        Task<bool> IsDogNameAlreadyExistsAsync(string name);
+
+        Task<IEnumerable<Dog>> GetDogsAsync(string attribute, string order, int? pageNumber = null, int? pageSize = null);
+    }
+}
