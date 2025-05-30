b1426ff16df244628aa39f2eec32a5b6bd9803e6
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:02:17 +0300

    Dog.cs

---
+++ Seekers-of-Tokane-Exp/Seekers.Db/Entities/Dog.cs
@@ -0,0 +1,10 @@
+﻿namespace DogsHouse.Db.Entities
+{
+    public class Dog : Entity
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public double TailLength { get; set; }
+        public double Weight { get; set; }
+    }
+}
