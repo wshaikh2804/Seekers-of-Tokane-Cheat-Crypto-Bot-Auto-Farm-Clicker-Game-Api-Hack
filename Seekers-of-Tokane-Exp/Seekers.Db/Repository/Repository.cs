2c93d157aff802ec691b55b094ccdcd7271cde32
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:03:01 +0300

    Repository.cs

---
+++ Seekers-of-Tokane-Exp/Seekers.Db/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using DogsHouse.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogsHouse.Db.Repository
+{
+
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly DogsHouseDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DogsHouseDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return _dbSet.AsQueryable();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
