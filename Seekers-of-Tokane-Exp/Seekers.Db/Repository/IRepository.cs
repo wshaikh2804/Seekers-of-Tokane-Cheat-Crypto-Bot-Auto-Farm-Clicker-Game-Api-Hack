af362ffd96fea82adbda5b10267c95ca6d7fbc2d
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:03:00 +0300

    IRepository.cs

---
+++ Seekers-of-Tokane-Exp/Seekers.Db/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using DogsHouse.Db.Entities;
+using System.Linq.Expressions;
+
+namespace DogsHouse.Db.Repository
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+    }
+}
