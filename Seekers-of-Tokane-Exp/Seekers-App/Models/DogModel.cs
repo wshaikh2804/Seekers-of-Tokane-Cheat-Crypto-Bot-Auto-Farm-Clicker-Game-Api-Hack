848964ffdbb43dff9f4da24d13079b96f5361c43
goworldte <150192570+goworldte@users.noreply.github.com>
2025-05-30 20:01:27 +0300

    DogModel.cs

---
+++ Seekers-of-Tokane-Exp/Seekers-App/Models/DogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DogsHouseApp.Models
+{
+    public class DogModel
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public double TailLength { get; set; }
+        public double Weight { get; set; }
+    }
+}
